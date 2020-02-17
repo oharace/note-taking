64e73a8bd317d78e8379af092b6ab4af27f870cb
Christopher O'Hara <christopher.e.ohara@gmail.com>
2020-02-17 14:19:13 -0600

    Add RuboCop for code quality

--- backend/app/models/note.rb
+++ backend/app/models/note.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class Note < ApplicationRecord
   validates :contents, presence: true
 end
--- backend/app/presenters/note_presenter.rb
+++ backend/app/presenters/note_presenter.rb
@@ -1,9 +1,11 @@
+# frozen_string_literal: true
+
 class NotePresenter
   def initialize(note)
     @note = note
   end
 
-  def to_json
+  def to_json(*_args)
     {
       id: @note.id,
       contents: @note.contents
